065bdc3faa2ff3c51177eca456b5cab4ba93f72e
ely colmenarez <elymelecc084@gmail.com>
2026-02-20 15:22:55 -0400

    trabajando con fecha y horas.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,5 +16,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+moment = Moment(app)
 
 from app import routes, models, errors
